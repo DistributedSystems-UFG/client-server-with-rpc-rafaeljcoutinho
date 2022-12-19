d8749961b2d84c3742580bcbe307d0350d77b299
Rafael Junqueira Coutinho <69867710+rafaeljcoutinho@users.noreply.github.com>
2022-12-19 11:49:43 -0300

    Update client.py

--- client.py
+++ client.py
@@ -6,4 +6,27 @@ class Client:
   print (conn.root.exposed_value())
   conn.root.exposed_append(5)       # Call an exposed operation,
   conn.root.exposed_append(6)       # and append two elements
-  print (conn.root.exposed_value())   # Print the result
+  conn.root.exposed_append(2)
+  conn.root.exposed_append(23)
+  conn.root.exposed_append(54)
+
+
+
+  print ("lista atual:")
+  print ([conn.root.exposed_value()])
+
+  conn.root.exposed_sort()
+
+  print("lista ordenada:")
+  print([conn.root.exposed_value()])
+
+  conn.root.exposed_remove(2)
+  conn.root.exposed_remove(2)
+
+  print("lista atual, com remocoes '2' '2':")
+  print([conn.root.exposed_value()])
+
+  conn.root.exposed_insert(2,55)
+
+  print("lista atual, com insercao '55' '2':")
+  print([conn.root.exposed_value()])
